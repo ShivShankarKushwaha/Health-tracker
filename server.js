6a2c5617402e168127105cf39331c2b0bfcbe50b
ShivshankarKushwaha <shivshankarkushwaha0000@gmail.com>
2023-10-28 22:53:33 +0530

    google login android

--- server.js
+++ server.js
@@ -200,7 +200,38 @@ app.post("/login", async (req, res) =>
     }
     res.status(404).json({ status: "User Not found" });
 });
-
+app.post("/androidgooglesign", async (req, res) =>
+{
+    try {
+        if(process.env.API!==req.body.userInfo.API)
+        {
+            return res.status(300).json({message:'UnAuthorized access'});
+        }
+        let data = req.body.userInfo;
+        const { name, email } = data.user;
+        let responce = await User.findOne({ email: email });
+        if (responce) {
+            req.session.email = email;
+            let token = signjwt(email);
+            return res.status(200).json({ message: 'user logged in', token: token })
+        }
+        else {
+            let pass =await PinGenerator();
+            let password =encryption(pass);
+            let user = new User({
+                name: name,
+                email: email,
+                password:password,
+            });
+            await user.save();
+            req.session.email = email;
+            let token = signjwt(email);
+            return res.status(200).json({ message: 'user signed up', token: token })
+        }
+    } catch (error) {
+        return res.status(300).json({ message: 'Something error occured', error })
+    }
+})
 app.get("/user", (req, res) =>
 {
     if (req.session.user) {
